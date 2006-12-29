2b83317f671c57cec9ee8a268feb1d66c8a88e82
sharplog <sharplog@c16174ff-a625-0410-88e5-87db5789ed7d>
2006-12-29 08:17:45 +0000

    Copied remotely

---
+++ trunk/src/demo/injectBeanImpl/LowerAction.h
@@ -0,0 +1,18 @@
+#ifndef DEMO_LOWERACTION_H
+#define DEMO_LOWERACTION_H
+
+#include <string>
+#include "Action.h"
+
+using namespace std;
+
+class LowerAction: public Action{
+private:
+	string Message;
+
+public:
+	const char* getMessage();
+	void  setMessage(const char* msg);
+	const char* excute(const char* s);
+};
+#endif
