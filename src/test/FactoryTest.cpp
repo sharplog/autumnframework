240ca660c695139afa04d6d92437c854871993d6
sharplog <sharplog@c16174ff-a625-0410-88e5-87db5789ed7d>
2007-03-13 10:54:02 +0000

    为bean增加工厂属性：factory-bean，并实现之。

--- src/test/FactoryTest.cpp
+++ src/test/FactoryTest.cpp
@@ -19,15 +19,18 @@ void FactoryTest::testInstanceFacotry()
 	IProductB* pb = (IProductB*)this->bf->getBean("ProductB1");
 	IProductC* pc = (IProductC*)this->bf->getBean("ProductC1");
 	IProductA* pa2 = (IProductA*)this->bf->getBean("ProductA2");
+	IProductB* pb2 = (IProductB*)this->bf->getBean("ProductB2");
 	
 	CPPUNIT_ASSERT(pa->getName().compare("ProductA_1_name_A1") == 0 );
 	CPPUNIT_ASSERT(pb->getName().compare("ProductB_1_name_B1") == 0 );
 	CPPUNIT_ASSERT(pc->getName().compare("ProductC_1_name_C1") == 0 );
 	CPPUNIT_ASSERT(pa2->getName().compare("ProductA_2_name_A2") == 0 );
+	CPPUNIT_ASSERT(pb2->getName().compare("ProductB_2_name_B2") == 0 );
 	this->bf->freeBean(pa);
 	this->bf->freeBean(pb);
 	this->bf->freeBean(pc);
 	this->bf->freeBean(pa2);
+	this->bf->freeBean(pb2);
 }
 
 void FactoryTest::testStaticFacotryMethod()
