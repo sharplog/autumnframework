7d9ff744e77d2fb9fb3fa26f40c0f24f5f691674
sharplog <sharplog@c16174ff-a625-0410-88e5-87db5789ed7d>
2008-07-17 17:55:07 +0000

    char* 改为string

--- src/demo/include/Action.h
+++ src/demo/include/Action.h
@@ -1,9 +1,11 @@
 #ifndef DEMO_ACTION_H
 #define DEMO_ACTION_H
 
+#include <string>
+
 class Action {
 public:
-	virtual const char* excute(const char* s) = 0;
+	virtual std::string excute(const char* s) = 0;
 };
 #endif
 
